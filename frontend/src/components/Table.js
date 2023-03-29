66ddef78fe5a4b4fceb63e05e6e8631e262ba160
Rafael Goos <rafagoos007@gmail.com>
2023-03-29 03:28:22 -0300

    Ajuste Table.js

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -7,16 +7,16 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(id, name, email) {
+  return { id, name, email };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData(1, 'Teste', 'teste@teste.com'),
+  createData(2, 'Rafael', 'Rafael@teste.com'),
+  createData(3, 'Leandro', 'Leandro@teste.com'),
+  createData(4, 'Carlos', 'Carlos@teste.com'),
+  createData(5, 'Henrique', 'Henrique@teste.com'),
 ];
 
 export default function BasicTable() {
@@ -33,14 +33,14 @@ export default function BasicTable() {
         <TableBody>
           {rows.map((row) => (
             <TableRow
-              key={row.name}
+              key={row.id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {row.id}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.name}</TableCell>
+              <TableCell align="right">{row.email}</TableCell>
 
             </TableRow>
           ))}
